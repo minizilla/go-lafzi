3cca9ae0cb7aa97b086c8ba033ccbd81c94cf0e4
Billy Zaelani Malik <m.billyzaelani@gmail.com>
2018-07-10 18:52:14 +0700

    Add cmd/generatecorpus for generating corpus

---
+++ cmd/generatecorpus/generatecorpus.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/billyzaelani/go-lafzi/phonetic/arabic"
+)
+
+var vowel = flag.Bool("v", true, "if true generate corpus with vowel otherwise generate corpus without vowel, default true")
+
+func main() {
+	flag.Parse()
+	docs, err := os.Open("data/quran/uthmani.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var targetFile string
+
+	if *vowel == true {
+		targetFile = "data/index/phonetic_vowel.txt"
+	} else {
+		targetFile = "data/index/phonetic.txt"
+	}
+
+	f, err := os.Create(targetFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	count, id := 0, 1
+	limit, i := 8000, 1
+
+	sc := bufio.NewScanner(docs)
+	fWriter := bufio.NewWriter(f)
+
+	// profiling
+	timeStart := time.Now()
+	for sc.Scan() {
+		// split delim "|"
+		// [0] = surat number
+		// [1] = surat name
+		// [2] = ayat number
+		// [3] = ayat text
+		data := bytes.Split(sc.Bytes(), []byte("|"))
+		var phonetic bytes.Buffer
+		encoder := arabic.NewEncoder(&phonetic)
+		encoder.SetLettersMode(arabic.LettersUthmani)
+		encoder.SetHarakat(*vowel)
+		if err := encoder.Encode(data[3]); err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%d. Processing surat {%s} ayat {%s}\n", id, data[0], data[2])
+		fmt.Fprintf(fWriter, "%d|%s\n", id, phonetic.String())
+		count++
+		id++
+
+		if i >= limit {
+			break
+		}
+		i++
+	}
+
+	err = fWriter.Flush()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	timeEnd := time.Now()
+	timeElapsed := timeEnd.Sub(timeStart)
+
+	// fmt.Printf("Total: %d\n\n", count)
+	fmt.Printf("\nProcessed in %f second\n", timeElapsed.Seconds())
+	fmt.Printf("Save file in:\n-%s\n", targetFile)
+}
