6b0428f0e7a5844688db6a3a7254dc38e0c3a28b
Billy Zaelani Malik <m.billyzaelani@gmail.com>
2018-07-17 08:06:54 +0700

    Add support for automatic phonetic encoding

--- cmd/search/search.go
+++ cmd/search/search.go
@@ -5,14 +5,20 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
+
+	"github.com/billyzaelani/go-lafzi/phonetic/indonesia"
 
 	"github.com/billyzaelani/go-lafzi/index"
-	"github.com/billyzaelani/go-lafzi/phonetic/indonesia"
+	"github.com/billyzaelani/go-lafzi/phonetic/latin"
 )
 
+var auto = flag.Bool("auto", true, "phonetic encoding for query")
 var q = flag.String("q", "", "query")
 
 func main() {
+	timeStart := time.Now()
+
 	flag.Parse()
 	termlist, err := os.Open("data/index/termlist.txt")
 	if err != nil {
@@ -22,12 +28,24 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	generatedLetters, err := os.Open("data/letters/generated.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	defer postlist.Close()
 
-	var encoder indonesia.Encoder
+	var latinEncoder latin.Encoder
+	var indonesiaEncoder indonesia.Encoder
 
-	idx := index.NewIndex(&encoder, termlist, postlist)
+	var idx *index.Index
+	if *auto {
+		latinEncoder.Parse(generatedLetters)
+		idx = index.NewIndex(&latinEncoder, termlist, postlist)
+	} else {
+		idx = index.NewIndex(&indonesiaEncoder, termlist, postlist)
+	}
+
 	idx.ParseTermlist()
 	phoneticCode, docs := idx.Search([]byte(*q))
 	fmt.Printf("query:\t%s\n", *q)
@@ -37,4 +55,9 @@ func main() {
 		fmt.Printf("matched terms: %v\n", doc.MatchedTerms)
 		fmt.Printf("LIS: %v\n\n", doc.LIS)
 	}
+
+	timeEnd := time.Now()
+	timeElapsed := timeEnd.Sub(timeStart)
+
+	fmt.Printf("Processed in %f second\n", timeElapsed.Seconds())
 }
